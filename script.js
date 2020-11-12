7acbab5707ea27450934cbac5e03d7b36d6883ab
brians-123 <brian_shor@hotmail.com>
2020-11-11 21:42:41 -0800

    working callback to taxee

--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 //Beginning of what David is building
-var ticker = "aapl";
-var queryURL =
-  "https://cloud.iexapis.com/stable/stock/" +
-  ticker +
-  "/batch?types=quote,news&range=1m&last=10&token=pk_57959b017ffa47508ac2c7eff69d3b05";
+// var ticker = "aapl";
+// var queryURL =
+//   "https://cloud.iexapis.com/stable/stock/" +
+//   ticker +
+//   "/batch?types=quote,news&range=1m&last=10&token=pk_57959b017ffa47508ac2c7eff69d3b05";
 
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (response) {
-  // var results = response.data;
-  // console.log(results);
-  console.log(response);
-});
+// $.ajax({
+//   url: queryURL,
+//   method: "GET",
+// }).then(function (response) {
+//   // var results = response.data;
+//   // console.log(results);
+//   console.log(response);
+// });
 
-console.log("test");
+// console.log("test");
 
 //end of what David is building
 
 //beginning of what Brian is building
 //send year to taxee.io and get tax brackets back
+// curl 'https://taxee.io/api/v2/federal/2020' -H 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBUElfS0VZX01BTkFHRVIiLCJodHRwOi8vdGF4ZWUuaW8vdXNlcl9pZCI6IjVmYWNhNGRiNTk0MGFlNjcxZGQwNDIzMiIsImh0dHA6Ly90YXhlZS5pby9zY29wZXMiOlsiYXBpIl0sImlhdCI6MTYwNTE0OTkxNX0.R0Czpcjn1O7E5vKecPOcDO8W6A5-KXF1I1U4xdxMhKA'
+
 var taxQueryURL = "https://taxee.io/api/v2/federal/2020";
+// $.ajax({
+//   url: taxQueryURL,
+//   method: "GET",
+// }).then(function (response) {
+//   //taxee.io/api/v2/federal/2020
+//   // var results = response.data;
+//   // console.log(results);
+//   https: console.log(response);
+// });
+
 $.ajax({
-  url: taxQueryURL,
+  url: "https://taxee.io/api/v2/federal/2020",
+  headers: {
+    Authorization:
+      "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBUElfS0VZX01BTkFHRVIiLCJodHRwOi8vdGF4ZWUuaW8vdXNlcl9pZCI6IjVmYWNhNGRiNTk0MGFlNjcxZGQwNDIzMiIsImh0dHA6Ly90YXhlZS5pby9zY29wZXMiOlsiYXBpIl0sImlhdCI6MTYwNTE0OTkxNX0.R0Czpcjn1O7E5vKecPOcDO8W6A5-KXF1I1U4xdxMhKA",
+  },
   method: "GET",
 }).then(function (response) {
-  //taxee.io/api/v2/federal/2020
-  // var results = response.data;
-  // console.log(results);
-  https: console.log(response);
+  console.log(response);
 });
 
 console.log("test");
