2880d553d4a4f35a814aaa9b1e9e12b4b02d1409
David Vargas <davidvargas95@gmail.com>
2020-11-11 19:57:59 -0800

    Added Stock API call to the JS file

---
+++ script.js
@@ -0,0 +1,15 @@
+var ticker = "aapl";
+var queryURL = "https://cloud.iexapis.com/stable/stock/" + ticker + "/batch?types=quote,news&range=1m&last=10&token=pk_57959b017ffa47508ac2c7eff69d3b05";
+
+
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+    .then(function(response) {
+
+        // var results = response.data;
+        // console.log(results);
+        console.log(response);
+    });
